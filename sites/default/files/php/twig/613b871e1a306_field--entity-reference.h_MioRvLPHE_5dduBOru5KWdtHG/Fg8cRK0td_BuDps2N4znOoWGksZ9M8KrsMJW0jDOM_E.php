4ff594eacddedd237250f27d224193fae6dd61bb
matanna <mat.bonhommeau85@gmail.com>
2021-09-10 18:33:21 +0200

    Add sliders on homepage and build contact form on the footer

---
+++ sites/default/files/php/twig/613b871e1a306_field--entity-reference.h_MioRvLPHE_5dduBOru5KWdtHG/Fg8cRK0td_BuDps2N4znOoWGksZ9M8KrsMJW0jDOM_E.php
@@ -0,0 +1,160 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/contrib/vani/templates/field/field--entity-reference.html.twig */
+class __TwigTemplate_1594869a41a3b76f8ceb693877a9721d228a2b855f539d90a7d7b4ee6f9f100c extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453 = $this->extensions["Drupal\\webprofiler\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->enter($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "themes/contrib/vani/templates/field/field--entity-reference.html.twig"));
+
+        // line 23
+        echo "<div class=\"node-taxonomy-container\">
+  ";
+        // line 24
+        if ( !($context["label_hidden"] ?? null)) {
+            // line 25
+            echo "    <h3";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["title_attributes"] ?? null), "addClass", [0 => "term-title"], "method", false, false, true, 25), 25, $this->source), "html", null, true);
+            echo "><i class=\"icon-hashtag theme-color\"></i> ";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["label"] ?? null), 25, $this->source), "html", null, true);
+            echo "</h3>
+  ";
+        }
+        // line 27
+        echo "  <ul class=\"taxonomy-terms\">
+    ";
+        // line 28
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["items"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["item"]) {
+            // line 29
+            echo "      <li";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["item"], "attributes", [], "any", false, false, true, 29), "addClass", [0 => "taxonomy-term"], "method", false, false, true, 29), 29, $this->source), "html", null, true);
+            echo ">";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["item"], "content", [], "any", false, false, true, 29), 29, $this->source), "html", null, true);
+            echo "</li>
+    ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['item'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 31
+        echo "  </ul>
+</div> <!--/.node-taxonomy-container -->
+";
+        
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->leave($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/contrib/vani/templates/field/field--entity-reference.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  73 => 31,  62 => 29,  58 => 28,  55 => 27,  47 => 25,  45 => 24,  42 => 23,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Theme override for  term fields.
+ *
+ * Available variables:
+ * - attributes: HTML attributes for the containing element.
+ * - label_hidden: Whether to show the field label or not.
+ * - title_attributes: HTML attributes for the label.
+ * - label: The label for the field.
+ * - content_attributes: HTML attributes for the content.
+ * - items: List of all the field items. Each item contains:
+ *   - attributes: List of HTML attributes for each item.
+ *   - content: The field item's content.
+ * - entity_type: The entity type to which the field belongs.
+ * - field_name: The name of the field.
+ * - field_type: The type of the field.
+ * - label_display: The display settings for the label.
+ *
+ * @see template_preprocess_field()
+ */
+#}
+<div class=\"node-taxonomy-container\">
+  {% if not label_hidden %}
+    <h3{{ title_attributes.addClass('term-title') }}><i class=\"icon-hashtag theme-color\"></i> {{ label }}</h3>
+  {% endif %}
+  <ul class=\"taxonomy-terms\">
+    {% for item in items %}
+      <li{{ item.attributes.addClass('taxonomy-term') }}>{{ item.content }}</li>
+    {% endfor %}
+  </ul>
+</div> <!--/.node-taxonomy-container -->
+", "themes/contrib/vani/templates/field/field--entity-reference.html.twig", "/var/www/html/onadetbeaute/themes/contrib/vani/templates/field/field--entity-reference.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("if" => 24, "for" => 28);
+        static $filters = array("escape" => 25);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if', 'for'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
